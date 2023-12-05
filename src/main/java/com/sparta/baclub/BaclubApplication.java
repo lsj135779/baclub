b7829549b2d1652d44daf31cd737be2aed257657
이승준 <lsj135779@gmail.com>
2023-12-05 13:39:51 +0900

    commit init

---
+++ src/main/java/com/sparta/baclub/BaclubApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.baclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaclubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaclubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/baclub/BaclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.baclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaclubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
