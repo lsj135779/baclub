f59699de96bf770db94c88c2368eef7b6e98fc58
Yoosup Shin <brian96shin@gmail.com>
2023-12-07 18:55:18 +0900

    fix: CommonResponseDto 어노테이션 수정

--- src/main/java/com/sparta/baclub/CommonResponseDto.java
+++ src/main/java/com/sparta/baclub/CommonResponseDto.java
@@ -1,5 +1,7 @@
 package com.sparta.baclub;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,13 +9,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL) //parsing할때 null값은 알아서 제외
 public class CommonResponseDto {
     private String msg;
     private Integer statusCode;
-
-    public CommonResponseDto(String message, int value) {
-        this.msg = message;
-        this.statusCode = value;
-    }
-
 }
