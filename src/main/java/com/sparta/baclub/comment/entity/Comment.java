b7892535b206fa0845938f0256c528e45864330c
이승준 <lsj135779@gmail.com>
2023-12-08 15:15:47 +0900

    fix: comment 패키지 복구

---
+++ src/main/java/com/sparta/baclub/comment/entity/Comment.java
@@ -0,0 +1,45 @@
+package com.sparta.baclub.comment.entity;
+
+import com.sparta.baclub.Timestamped;
+import com.sparta.baclub.comment.dto.CommentRequestDto;
+import com.sparta.baclub.comment.dto.CommentResponseDto;
+import com.sparta.baclub.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Table
+@Entity
+@NoArgsConstructor
+public class Comment extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String content;
+
+    // post, user 관계설정하기
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Comment(Post post, CommentRequestDto commentRequestDto, User user) {
+        this.content = commentRequestDto.getContent();
+        // post 정보 저장하기
+        this.post = post;
+        // user 정보 저장하기
+        this.user = user;
+    }
+
+
+    public CommentResponseDto update(CommentRequestDto commentRequestDto) {
+        this.content = commentRequestDto.getContent();
+        return new CommentResponseDto(this);
+    }
+}
