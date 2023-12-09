1063c9cbdf0155dff147f87cdf227d6e4565a87c
Yoosup Shin <brian96shin@gmail.com>
2023-12-09 20:52:34 +0900

    Refactor: Nickname 정규표현식으로 검증

--- src/main/java/com/sparta/baclub/user/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/baclub/user/dto/SignupRequestDto.java
@@ -9,12 +9,16 @@ import lombok.Setter;
 @Setter
 public class SignupRequestDto {
     @NotBlank
-    @Pattern(regexp = "^[a-z0-9]{4,10}$")
+    @Pattern(regexp = "^[a-z0-9]{4,10}$",
+             message = "아이디는 영문+숫자를 포함한 4~10자리여야 합니다.")
     private String username;
     @NotBlank
-    @Pattern(regexp = "^[a-zA-Z\\d`~!@#$%^&*()-_=+]{8,15}$")
+    @Pattern(regexp = "^[a-zA-Z\\d`~!@#$%^&*()-_=+]{8,15}$",
+             message = "비밀번호는 영문+숫자+특수문자를 포함한 8~15자리여야 합니다.")
     private String password;
     @NotBlank
+    @Pattern(regexp = "[a-zA-Z0-9가-힣\\\\s]{2,15}$",
+             message = "닉네임은 영문자, 한글, 공백 포함 2~15글자까지 가능합니다.")
     private String nickname;
     @NotBlank
     private String phoneNumber;
